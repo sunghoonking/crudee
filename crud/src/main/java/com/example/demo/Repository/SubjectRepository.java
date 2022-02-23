36e433ac9fb97797e913dbffcb0588d9f39ef9e2
sunghoonkim <90130141+sunghoonking@users.noreply.github.com>
2022-02-23 17:51:30 +0900

    Delete crud directory

--- crud/src/main/java/com/example/demo/Repository/SubjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-import com.example.demo.models.Subject;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-
-public interface SubjectRepository extends JpaRepository<Subject,Long> {
-}
